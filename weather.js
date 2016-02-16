d020d8a333d0a79e6439e6935204d6d389b64946
Bopanna Mechanda Appanna <bappanna@sapient.com>
2016-02-16 12:33:35 +0530

    version2

--- weather.js
+++ weather.js
@@ -18,9 +18,32 @@ var weather = function() {
             body = JSON.parse(body);
             //console.log(body);
 
-            console.log('Highest Temp:', body.current_observation.temp_c);
+            console.log('Temperature Outside:', body.current_observation.temp_c + ' C');
+            console.log('Weather is :', body.current_observation.weather);
 
-            console.log('Weather is :', body.current_observation.weather)
+			if (body.current_observation.temp_c >31 )
+			{
+				console.log('Day seem to be sunny!!', body.current_observation.temp_c);
+			}
+			else if (body.current_observation.temp_c >34) 
+			{
+				console.log('Its Very hot day, Dont dare to wear jackets !', body.current_observation.temp_c);
+
+			}
+			else if ( body.current_observation.weather === "Partly Cloudy") 
+			{
+				console.log('Its may rain, have umbrella handy');
+			}
+			else if (body.current_observation.weather ==="Clear") 
+			{
+				console.log('Good to gooooo');
+			}
+
+			else if (body.current_observation.temp_c < 25) 
+			{
+				console.log('Weather is super cool..Just Relaxxx');
+			}
+			
 
         } catch (e) {
             console.log('error occured', e);
@@ -31,5 +54,5 @@ var weather = function() {
 
 }
 
-// weather();
+weather();
 module.exports = weather;
